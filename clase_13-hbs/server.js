74110fca76f83487e3061fef4006f2810bc68314
Berni <pbernal@mendoza.edu.ar>
2025-05-09 19:54:36 -0300

    App con conexión lista

--- clase_13-hbs/server.js
+++ clase_13-hbs/server.js
@@ -9,15 +9,18 @@ dotenv.config(); // configuramos dotenv
 // Importamos el servidor
 const app = require('./app'); // importamos el servidor
 
+// Importamos la conexión a la base de datos
+const connectDB = require('./conexion/conecctionMongo'); // importamos la conexión a la base de datos
+
 // Importamos el puerto
 const PORT = process.env.PORT || 3000; // importamos el puerto de la configuración de dotenv o el puerto 3000 por defecto
 
+// Importamos el URI de la base de datos
+const MONGO_URI = process.env.MONGO_LOCAL; // importamos el URI de la base de datos de la configuración de dotenv
+
 
 // Conexión a la base de datos
-
-
-
-
+connectDB(MONGO_URI); // conectamos a la base de datos
 
 
 // levantamos el servidor
